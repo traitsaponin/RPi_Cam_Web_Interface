55c0921c8913bcc6a524c6114f14086e370976d0
traitsaponin <trait.saponin_0p@icloud.com>
2024-10-05 10:03:39 +1000

    Updating RasPyCam to v119

--- etc/raspycam/_internal/core/process.py
+++ etc/raspycam/_internal/core/process.py
@@ -288,7 +288,6 @@ def start_background_process(config_filepath):
 
         time.sleep(0.01)  # Small delay before next iteration
 
-    print("Shutting down gracefully...")
     cam.current_status = "halted"
     cmd_processing_thread.join()  # Wait for command processing thread to finish
     for t in threads:
